42f4903934130defe0fa54afebe4eebb8513ec29
azaza <zador94@mail.ru>
2022-10-23 17:01:28 +0300

    FinalVersionProgramm

--- DirectoryFiles/Program.cs
+++ DirectoryFiles/Program.cs
@@ -1,5 +1,38 @@
 ﻿
+using DirectoryFiles;
+
+Console.WriteLine("Введите название директории");
 
 DirectoryInfo dir = new DirectoryInfo($"C:\\"+ Console.ReadLine());
+
+if (!dir.Exists)
+{
+    dir.Create();
+}
+
+Console.WriteLine("Введите название файла");
+
+string nameFile = Console.ReadLine();
+
+DirectoryInfo dir1 = dir.CreateSubdirectory(nameFile.Substring(0,1));
+
+FileInfo file = new FileInfo(dir1 + $"\\{nameFile}.txt");
+
+FileDirOperation.WriteSile(file);
+
+
+
+Console.WriteLine("Введите название нового файла");
+
+string newnamefile = Console.ReadLine();
+
+DirectoryInfo dir2 = dir.CreateSubdirectory(newnamefile.Substring(0,1));
+
+FileInfo newFile = new FileInfo(dir2 + $"\\{newnamefile}.txt");
+
+file.MoveTo(dir2 + $"\\{newnamefile}.txt");
+
+Console.WriteLine("Данные перезаписаны");
+
 
 
